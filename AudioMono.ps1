e1e36474f0485618c5a3476140db8b07bf920ca0
mcaballero879 <mcaballero879@gmail.com>
2023-09-15 23:23:37 -0300

    Audio mono

---
+++ AudioMono.ps1
@@ -0,0 +1,37 @@
+﻿
+
+function ActivarMono(){
+powershell -windowstyle hidden -command "Start-Process cmd -ArgumentList '/s,/c,REG ADD "HKCU\Software\Microsoft\Multimedia\Audio" /V AccessibilityMonoMixState /T REG_DWORD /D 1 /F & net stop "Audiosrv" & net start "Audiosrv"' -Verb runAs"
+if ($? -eq "True"){
+
+Write-Host -ForegroundColor Green "Se activo el audio Mono"
+pause}
+
+ else { Write-Host -ForegroundColor Red "Ocurrio un error, vuelva a ejcutar el programa"
+pause}
+
+
+
+                        }
+#ir al registro
+
+function verificarRegistroMono {
+
+
+cd HKCU:
+
+if (Get-Item .\Software\Microsoft\Multimedia\Audio | Where-Object {$_.Property -contains "AccessibilityMonoMixState"})
+        {Write-Host -ForegroundColor Green "El audio ya está en mono"
+            pause
+            exit
+            
+            }
+else {Write-Host -ForegroundColor Red "Activando Audio Mono"
+        ActivarMono
+        
+        }
+
+
+}
+
+verificarRegistroMono
