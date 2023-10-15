faff3c08f7e7b6f08a4fa24f3ca1483523ac8dec
mcaballero879 <mcaballero879@gmail.com>
2023-10-15 19:30:03 -0300

    Agregué script compara dia

---
+++ dia.ps1
@@ -0,0 +1,16 @@
+﻿$dia = (get-date).AddDays(-7)
+
+$dia.ToString("ddMMyyyy")
+
+
+
+
+
+$ARCHIVOS = dir C:\Users\Marcelo\Desktop | Select-Object Name,LastWriteTime
+foreach ($A in $ARCHIVOS){
+
+              Write-Host ($A.LastWriteTime).ToString("dd/MM/yyyy")
+              if (    ($A.LastWriteTime).ToString("dd/MM/yyyy") -eq "$($dia.ToString("dd/MM/yyyy"))" ) { Write-Host -ForegroundColor Green "el archivo $($A.Name) tiene el mismo dia"}
+              else { Write-Host -ForegroundColor Red "el archivo $($A.Name) NO tiene el mismo dia"}
+
+}
